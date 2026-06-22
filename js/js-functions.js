beb4d373bddb2b8a47a495087264a397a73251ba
Vaman Deshmukh <vamandeshmukh@gmail.com>
2026-06-22 11:28:09 +0530

    js intro

---
+++ js/js-functions.js
@@ -0,0 +1,18 @@
+// Functions in JS 
+// ===============
+
+
+// Old JS function
+function fun1() {
+    console.log('fun1 function called.');
+}
+
+fun1();
+
+// Modern JS function 
+const fun2 = () => {
+    console.log('fun2 function called.');
+};
+
+fun2();
+
