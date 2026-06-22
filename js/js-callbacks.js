dca07128498c6a09a9685d0d1041878aa1a1301a
Vaman Deshmukh <vamandeshmukh@gmail.com>
2026-06-22 12:35:36 +0530

    js intro

--- js/js-callbacks.js
+++ js/js-callbacks.js
@@ -1,12 +1,40 @@
 // Problem of async JS 
 
+// const getData = (arg) => {
+//     console.log('getData called');
+//     arg({ city: 'Bengaluru' });
+// };
+
+// getData((data) => {
+//     console.log('anonymous function called.');
+//     console.log(data.city);
+// });
+
+// problem of async js 
+// =====================
+
+// const getData = () => {
+//     console.log('getData called');
+//     setTimeout(() => {
+//         return { city: 'Bengaluru' };
+//     }, 2000);
+// };
+
+// const output = getData();
+// // const output = undefined;
+// console.log(output.city);
+
+// solution 1 - callback 
+// =====================
+
 const getData = (arg) => {
     console.log('getData called');
-    arg({ city: 'Bengaluru' });
+    setTimeout(() => {
+        arg({ city: 'Bengaluru' });
+    }, 2000);
 };
 
 getData((data) => {
     console.log('anonymous function called.');
     console.log(data.city);
 });
-
