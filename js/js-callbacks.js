5d521c3cf6fc7e2167866d880ebe768739356dc7
Vaman Deshmukh <vamandeshmukh@gmail.com>
2026-06-22 12:51:17 +0530

    js intro

--- js/js-callbacks.js
+++ js/js-callbacks.js
@@ -24,19 +24,42 @@
 // // const output = undefined;
 // console.log(output.city);
 
-// solution 1 - callback 
+// // solution 1 - callback 
+// // =====================
+
+// console.log("Start");
+
+// const getData = (arg) => {
+//     console.log('getData called');
+//     setTimeout(() => {
+//         arg({ city: 'Bengaluru' });
+//     }, 2000);
+// };
+
+// getData((data) => {
+//     console.log('anonymous function called.');
+//     console.log(data.city);
+// });
+
+
+// solution 1 - Promise  
 // =====================
 
 console.log("Start");
 
-const getData = (arg) => {
+const getData = () => {
     console.log('getData called');
-    setTimeout(() => {
-        arg({ city: 'Bengaluru' });
-    }, 2000);
+    const isDataAvailable = false; // true false 
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (isDataAvailable)
+                resolve({ city: 'Bengaluru' });
+            else
+                reject({ message: 'Data not available' });
+        }, 2000);
+    });
 };
 
-getData((data) => {
-    console.log('anonymous function called.');
-    console.log(data.city);
-});
+getData()
+    .then((response) => { console.log(response.city); })
+    .catch((error) => { console.log(error.message); });
