bbe6e5d15a637b66c59dca4df30b95aa8aa6a357
Vaman Deshmukh <vamandeshmukh@gmail.com>
2026-06-23 12:33:27 +0530

    node demos

--- js/js-dom-html.js
+++ js/js-dom-html.js
@@ -1,16 +1,36 @@
-// JS DOM Methods
+// // JS DOM Methods
 
-// const sayHi = () => {
-//     inputText = document.getElementById('username').value;
-//     document.getElementById('output').innerText = `Hi ${inputText}!`;
-// };
+// // const sayHi = () => {
+// //     inputText = document.getElementById('username').value;
+// //     document.getElementById('output').innerText = `Hi ${inputText}!`;
+// // };
 
-// addEventListener(arg1, arg2);
-// addEventListener('event-to-capture', () => {function-to-execute-on-that-event});
+// // document.getElementById("element-to-capture")
+// // .addEventListener(arg1, arg2);
+// // .addEventListener('event-to-capture', () => {function-to-execute-on-that-event});
+
+// document.getElementById("submit").addEventListener("click", function () {
+//     const name = document.getElementById("username").value;
+//     const output = document.getElementById("output");
+//     output.textContent = `Hi ${name}!`;
+// });
 
 
 document.getElementById("submit").addEventListener("click", function () {
     const name = document.getElementById("username").value;
     const output = document.getElementById("output");
-    output.textContent = `Hi ${name}!`;
+    // output.textContent = `Hi ${name}!`;
+    // alert(`Hi ${name}!`);
+    output.textContent = confirm("Are you sure?") ? 'Yes' : 'No';
 });
+
+
+
+
+
+
+
+
+
+
+
