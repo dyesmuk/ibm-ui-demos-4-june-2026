5480393fc9f530c537f477bcdd33def53fb301a3
Vaman Deshmukh <vamandeshmukh@gmail.com>
2026-06-23 12:48:12 +0530

    node demos

---
+++ node-projects/ibm-node-demo/src/calc.js
@@ -0,0 +1,8 @@
+// calc.js
+
+const addNums = (a, b) => {
+    console.log('calc.addNums called.');
+    return a + b;
+};
+
+export default addNums;
--- node-projects/ibm-node-demo/src/script.js
+++ node-projects/ibm-node-demo/src/script.js
@@ -1,1 +1,7 @@
+// script.js
+
+import addNums from "./calc.js";
+
 console.log('node project');
+const output = addNums(10, 20);
+console.log(output);
