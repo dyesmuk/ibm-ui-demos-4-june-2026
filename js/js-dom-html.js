aed4a81105c9ba60aefaf5a0766ea278b77b238d
Vaman Deshmukh <vamandeshmukh@gmail.com>
2026-06-23 11:42:51 +0530

    pushed

--- js/js-dom-html.js
+++ js/js-dom-html.js
@@ -1,3 +1,12 @@
 // JS DOM Methods
 
-console.log(document.getElementById('username'));
+const sayHi = () => {
+    inputText = document.getElementById('username').value;
+    document.getElementById('output').innerText = `Hi ${inputText}!`;
+};
+
+document.getElementById("submit").addEventListener("click", function () {
+    const name = document.getElementById("username").value;
+    const output = document.getElementById("output");
+    output.textContent = `Hi ${name}!`;
+});
