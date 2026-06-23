e83c7b3f2e426edae455de941ffb816e21224a42
Vaman Deshmukh <vamandeshmukh@gmail.com>
2026-06-23 14:58:23 +0530

    node demos

--- node-projects/ibm-node-demo/src/calc.js
+++ node-projects/ibm-node-demo/src/calc.js
@@ -1,43 +1,43 @@
-// calc.js
+// // calc.js
 
-// 1. only one export 
-// const addNums = (a, b) => {
-//     return a + b;
-// };
+// // 1. only one export
+// // const addNums = (a, b) => {
+// //     return a + b;
+// // };
 
-// export default addNums;
+// // export default addNums;
 
-// 2. many exports  
+// // 2. many exports
 
-// const addNums = (a, b) => {
-//     return a + b;
-// };
+// // const addNums = (a, b) => {
+// //     return a + b;
+// // };
 
-// const subNums = (a, b) => {
-//     return a + b;
-// };
+// // const subNums = (a, b) => {
+// //     return a + b;
+// // };
 
-// export { addNums, subNums };
+// // export { addNums, subNums };
 
-// 3. many exports  
+// // 3. many exports
 
-// export const addNums = (a, b) => {
-//     return a + b;
-// };
+// // export const addNums = (a, b) => {
+// //     return a + b;
+// // };
 
-// export const subNums = (a, b) => {
-//     return a + b;
-// };
+// // export const subNums = (a, b) => {
+// //     return a + b;
+// // };
 
 
-// 4. one export object   
+// // 4. one export object
 
-export const calc = {
-    addNums: (a, b) => {
-        return a + b;
-    },
+// export const calc = {
+//     addNums: (a, b) => {
+//         return a + b;
+//     },
 
-    subNums: (a, b) => {
-        return a + b;
-    }
-}
+//     subNums: (a, b) => {
+//         return a + b;
+//     }
+// }
--- node-projects/ibm-node-demo/src/script.js
+++ node-projects/ibm-node-demo/src/script.js
@@ -1,37 +1,59 @@
-// // script.js
+// // // script.js
 
-// 1. 
-// import addNums from "./calc.js";
+// // imports and exports 
+// // ===================
+// // 1. 
+// // import addNums from "./calc.js";
+
+// // console.log('node project');
+// // const output = addNums(10, 20);
+// // console.log(output);
+
+// // 2. 
+// // import { addNums, subNums } from "./calc.js";
+
+// // console.log('node project');
+// // const output = addNums(10, 20);
+// // console.log(output);
+
+// // console.log(subNums(10, 5));
+
+// // 3. 
+// // import { addNums, subNums } from "./calc.js";
+
+// // console.log('node project');
+// // const output = addNums(10, 20);
+// // console.log(output);
+
+// // console.log(subNums(10, 5));
+
+// // 4.
+// import calc from "./calc.js";
 
 // console.log('node project');
-// const output = addNums(10, 20);
+// const output = calc.addNums(10, 20);
 // console.log(output);
 
-// 2. 
-// import { addNums, subNums } from "./calc.js";
-
-// console.log('node project');
-// const output = addNums(10, 20);
-// console.log(output);
-
-// console.log(subNums(10, 5));
-
-// 3. 
-// import { addNums, subNums } from "./calc.js";
-
-// console.log('node project');
-// const output = addNums(10, 20);
-// console.log(output);
-
-// console.log(subNums(10, 5));
-
-// 4.
-import calc from "./calc.js";
-
-console.log('node project');
-const output = calc.addNums(10, 20);
-console.log(output);
-
-console.log(calc.subNums(10, 5));
+// console.log(calc.subNums(10, 5));
 
 
+// File System and CLAs 
+// ====================
+
+// fs module in NodeJS 
+
+
+import { readFileSync, readFile } from 'fs';
+
+// const data = readFileSync('./src/sample.txt', 'utf8');
+// console.log(data);
+
+readFile('./src/sample.txt', 'utf8', (err, data) => {
+    if (err) {
+        console.error('Error reading file:', err.message)
+        return
+    }
+    console.log(data)
+})
+
+console.log('This runs BEFORE the file is read! (async)')
