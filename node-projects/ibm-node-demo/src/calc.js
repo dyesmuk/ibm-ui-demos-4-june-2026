7742646df59494fc39d2666d645505a67282e172
Vaman Deshmukh <vamandeshmukh@gmail.com>
2026-06-23 14:33:50 +0530

    node demos

--- node-projects/ibm-node-demo/src/calc.js
+++ node-projects/ibm-node-demo/src/calc.js
@@ -1,8 +1,43 @@
 // calc.js
 
-const addNums = (a, b) => {
-    console.log('calc.addNums called.');
-    return a + b;
-};
+// 1. only one export 
+// const addNums = (a, b) => {
+//     return a + b;
+// };
 
-export default addNums;
+// export default addNums;
+
+// 2. many exports  
+
+// const addNums = (a, b) => {
+//     return a + b;
+// };
+
+// const subNums = (a, b) => {
+//     return a + b;
+// };
+
+// export { addNums, subNums };
+
+// 3. many exports  
+
+// export const addNums = (a, b) => {
+//     return a + b;
+// };
+
+// export const subNums = (a, b) => {
+//     return a + b;
+// };
+
+
+// 4. one export object   
+
+export const calc = {
+    addNums: (a, b) => {
+        return a + b;
+    },
+
+    subNums: (a, b) => {
+        return a + b;
+    }
+}
--- node-projects/ibm-node-demo/src/script.js
+++ node-projects/ibm-node-demo/src/script.js
@@ -1,7 +1,37 @@
-// script.js
+// // script.js
 
-import addNums from "./calc.js";
+// 1. 
+// import addNums from "./calc.js";
+
+// console.log('node project');
+// const output = addNums(10, 20);
+// console.log(output);
+
+// 2. 
+// import { addNums, subNums } from "./calc.js";
+
+// console.log('node project');
+// const output = addNums(10, 20);
+// console.log(output);
+
+// console.log(subNums(10, 5));
+
+// 3. 
+// import { addNums, subNums } from "./calc.js";
+
+// console.log('node project');
+// const output = addNums(10, 20);
+// console.log(output);
+
+// console.log(subNums(10, 5));
+
+// 4.
+import calc from "./calc.js";
 
 console.log('node project');
-const output = addNums(10, 20);
+const output = calc.addNums(10, 20);
 console.log(output);
+
+console.log(calc.subNums(10, 5));
+
+
