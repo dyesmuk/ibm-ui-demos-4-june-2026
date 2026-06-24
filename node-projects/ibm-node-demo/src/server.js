c376070c79865fd40b61b7d8207bedf88880fd43
Vaman Deshmukh <vamandeshmukh@gmail.com>
2026-06-24 15:15:04 +0530

    code updated

--- node-projects/ibm-node-demo/src/server.js
+++ node-projects/ibm-node-demo/src/server.js
@@ -6,9 +6,9 @@ import { Server } from 'socket.io';
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-
+    console.log('createServer started.');
     if (req.url === '/') {
-
+        console.log('/ requested.');
         const filePath = path.join(
             process.cwd(),
             'src',
@@ -17,18 +17,20 @@ const server = http.createServer((req, res) => {
         );
 
         fs.readFile(filePath, 'utf8', (err, data) => {
-
+            console.log('html accessed.');
             if (err) {
                 res.writeHead(500, {
                     'Content-Type': 'text/plain'
                 });
                 res.end('Error loading page');
+                console.log('html not found.');
                 return;
             }
 
             res.writeHead(200, {
                 'Content-Type': 'text/html'
             });
+            console.log('html started.');
 
             res.end(data);
         });
@@ -46,26 +48,22 @@ const server = http.createServer((req, res) => {
 const io = new Server(server);
 
 io.on('connection', (socket) => {
-
     console.log(`Client connected: ${socket.id}`);
 
     socket.on('chat-message', (msg) => {
-
         console.log(`Received: ${msg}`);
-
         io.emit('chat-message', msg);
     });
 
     socket.on('disconnect', () => {
-
         console.log(`Client disconnected: ${socket.id}`);
     });
 });
 
 server.listen(PORT, () => {
-
     console.log(`Server running at http://localhost:${PORT}`);
 });
+
 
 
 
