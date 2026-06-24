d4e4f1d97427d760cd3517a07c10a70bcbe33780
Vaman Deshmukh <vamandeshmukh@gmail.com>
2026-06-24 11:40:44 +0530

    code updated

--- node-projects/ibm-node-demo/src/mongoose-demo.js
+++ node-projects/ibm-node-demo/src/mongoose-demo.js
@@ -1,20 +1,47 @@
-// db.js
-import { MongoClient } from 'mongodb'
+import { MongoClient } from 'mongodb';
 
-const uri = process.env.MONGO_URI  // from .env
-const client = new MongoClient(uri)
+const uri = 'mongodb://localhost:27017';
 
-let db
+async function MongoDbCon() {
 
-const connect = async () => {
-    await client.connect()
-    db = client.db('myapp')
-    console.log('✅ Connected to MongoDB')
+    const client = new MongoClient(uri);
+
+    try {
+        await client.connect();
+        console.log('Connected!');
+        const db = client.db('ibm-ems');
+        const employees = db.collection('emps');
+        const employeeList = await employees.find().toArray();
+        employeeList.forEach(e => console.log(e.name));
+
+    } catch (error) {
+        console.error(error.message);
+    } finally {
+        await client.close();
+        console.log('Closed!');
+    }
 }
 
-const getDb = () => {
-    if (!db) throw new Error('DB not connected! Call connect() first.')
-    return db
-}
+MongoDbCon();
 
-export default { connect, getDb }
+// // db.js
+// import { MongoClient } from 'mongodb';
+
+// // const uri = process.env.MONGO_URI  // from .env
+// const uri = 'mongodb://localhost:27017/';
+// const client = new MongoClient(uri);
+
+// let db;
+
+// const connect = async () => {
+//     await client.connect();
+//     db = client.db('myapp');
+//     console.log('✅ Connected to MongoDB');
+// }
+
+// const getDb = () => {
+//     if (!db) throw new Error('DB not connected! Call connect() first.')
+//     return db;
+// }
+
+// export default { connect, getDb };
