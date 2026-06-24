5755c675c678827e29aded5a44a2f8679ace8397
Vaman Deshmukh <vamandeshmukh@gmail.com>
2026-06-24 16:18:00 +0530

    code updated

--- node-projects/ibm-node-demo/src/ems/emp-stuff.js
+++ node-projects/ibm-node-demo/src/ems/emp-stuff.js
@@ -6,16 +6,35 @@ const employees = [
     { id: 3, name: 'Tonu', salary: 70000 }
 ];
 
-export const calculateBonus = (salary) => {
-    return salary * 0.10;
-}
+export const calculateBonus = salary => salary * 0.10;
 
-export const getEmployees = () => {
-    return employees.map(emp => emp.name);
-}
+export const getEmployees = () => employees.map(emp => emp.name);
 
-export const findEmployee = (id) => {
-    return employees.find(emp => emp.id === id);
-}
+export const findEmployee = id => employees.find(emp => emp.id === id);
 
-export const addNums = (a, b) => { return a + b; }; 
+export const addNums = (a, b) => a + b;
+
+
+// // emp-stuff.js
+
+// const employees = [
+//     { id: 1, name: 'Sonu', salary: 50000 },
+//     { id: 2, name: 'Monu', salary: 60000 },
+//     { id: 3, name: 'Tonu', salary: 70000 }
+// ];
+
+// export const calculateBonus = (salary) => {
+//     return salary * 0.10;
+// }
+
+// export const getEmployees = () => {
+//     return employees.map(emp => emp.name);
+// }
+
+// export const findEmployee = (id) => {
+//     return employees.find(emp => emp.id === id);
+// }
+
+// export const addNums = (a, b) => {
+//     return a + b;
+// }; 
--- node-projects/ibm-node-demo/src/ems/emp-stuff.test.js
+++ node-projects/ibm-node-demo/src/ems/emp-stuff.test.js
@@ -2,5 +2,32 @@
 
 import { calculateBonus, getEmployees, findEmployee, addNums } from './emp-stuff.js';
 
+describe('ems tests suite', () => {
+
+    it('test addNums', () => {
+        const sum = addNums(10, 20);
+        expect(sum).toBe(30);
+    });
+
+    it('test addNums negative', () => {
+        const sum = addNums(10, 20);
+        expect(sum).not.toBe(35);
+    });
+});
+
+
+
+// describe(arg1, arg2);
+// describe('', () => { });
+// describe('ems tests suite', () => { });
+
+// describe('ems tests suite', () => {
+//     it();
+//     test();
+//  });
+
+// describe('ems tests suite', () => {
+//     it('test addNums', () => { });
+// });
 
 
