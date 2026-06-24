fe25d427ed92547ec32b224084588caa8dea3a9c
Vaman Deshmukh <vamandeshmukh@gmail.com>
2026-06-24 16:24:32 +0530

    code updated

--- node-projects/ibm-node-demo/src/ems/emp-stuff.test.js
+++ node-projects/ibm-node-demo/src/ems/emp-stuff.test.js
@@ -4,15 +4,32 @@ import { calculateBonus, getEmployees, findEmployee, addNums } from './emp-stuff
 
 describe('ems tests suite', () => {
 
-    it('test addNums', () => {
-        const sum = addNums(10, 20);
-        expect(sum).toBe(30);
+    describe('find employee by id tests', () => {
+
+        it('given id 1, name shoule be Sonu', () => {
+            expect(findEmployee(1).name).toBe('Sonu');
+        });
+
+        it('given id 1, name shoule NOT be Monu', () => {
+            expect(findEmployee(1).not.name).toBe('Monu');
+        });
     });
 
-    it('test addNums negative', () => {
-        const sum = addNums(10, 20);
-        expect(sum).not.toBe(35);
+
+
+    describe('demo tests', () => {
+
+        it('test addNums', () => {
+            const sum = addNums(10, 20);
+            expect(sum).toBe(30);
+        });
+
+        it('test addNums negative', () => {
+            const sum = addNums(10, 20);
+            expect(sum).not.toBe(35);
+        });
     });
+
 });
 
 
