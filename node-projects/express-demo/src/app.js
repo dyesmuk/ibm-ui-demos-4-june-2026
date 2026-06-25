5b74b8ed82717a07e14e4fb272dccdbc4273000a
Vaman Deshmukh <vamandeshmukh@gmail.com>
2026-06-25 09:33:47 +0530

    express demo

--- node-projects/express-demo/src/app.js
+++ node-projects/express-demo/src/app.js
@@ -1,1 +1,30 @@
+// app.js
+import express from 'express';
 
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    console.log('welcome');
+    res.send('Welcome page');
+});
+
+app.get('/about', (req, res) => {
+    console.log('about');
+    res.send('About page');
+});
+
+app.get('/employees', (req, res) => {
+    console.log('about');
+    res.send('Employees page');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+
+// app.get('/', () => { });
+// app.get(arg1, arg2);
+// app.listen(PORT);
+// app.listen(arg1, [arg2]);
