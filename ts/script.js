3afcb5ac5ebc1539b6e480a6b7b198fbd43dc56a
Vaman Deshmukh <vamandeshmukh@gmail.com>
2026-06-26 11:43:52 +0530

    updated code

--- ts/script.js
+++ ts/script.js
@@ -1,11 +1,16 @@
 "use strict";
-// console.log('TS Hello world!');
-// // // java
-// // // int num = 10;
-let salary = 90000.25;
+// // console.log('TS Hello world!');
+// // // // java
+// // // // int num = 10;
+// let salary = 90000.25;
+// console.log(salary);
+// // salary = 'abc';
+// // console.log(salary);
+// salary = 98000.50;
+// console.log(salary);
+// Type notation in TS 
+// =================== 
+let salary; //   Variable 'salary' implicitly has an 'any' type
+// let salary = 10.25; // type inference  
+salary = 'abc'; // Type 'string' is not assignable to type 'number'.ts(2322)
 console.log(salary);
-// salary = 'abc';
-console.log(salary);
-salary = 98000.50;
-console.log(salary);
-
--- ts/script.ts
+++ ts/script.ts
@@ -1,19 +1,54 @@
-// console.log('TS Hello world!');
+// // console.log('TS Hello world!');
 
-// // // java
-// // // int num = 10;
+// // // // java
+// // // // int num = 10;
 
-let salary = 90000.25;
-console.log(salary);
-
-// salary = 'abc';
+// let salary = 90000.25;
 // console.log(salary);
 
-salary = 98000.50;
-console.log(salary);
+// // salary = 'abc';
+// // console.log(salary);
+
+// salary = 98000.50;
+// console.log(salary);
+
+// Type notation in TS 
+// =================== 
+
+// let salary; //   Variable 'salary' implicitly has an 'any' type
+// // let salary = 10.25; // type inference  
+// salary = 'abc'; // Type 'string' is not assignable to type 'number'.ts(2322)
+// console.log(salary);
 
 
+// Type notation in TS 
+// =================== 
 
+// // java
+// // int num = 10;
+
+// let salary : number = 10.25; 
+// salary = 'abc'; // Type 'string' is not assignable to type 'number'.ts(2322)
+// console.log(salary);
+
+
+// let salary: number = 10.25;
+// // salary = 'abc'; // Type 'string' is not assignable to type 'number'.ts(2322)
+// salary = 12.50; // works
+// console.log(salary);
+
+const addNums = (a: number, b: number): number => {
+    return a + b;
+};
+
+console.log(addNums(2, 3));
+// console.log(addNums('abc', 'def')); // error 
+
+const addNumsPrint = (a: number, b: number): void => {
+    console.log(a + b);
+};
+
+console.log(addNumsPrint(2, 3));
 
 
 
