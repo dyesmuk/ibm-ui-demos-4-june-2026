af83e3d84ceb8f5785a464b4a35b090839645977
Vaman Deshmukh <vamandeshmukh@gmail.com>
2026-06-26 15:11:47 +0530

    updated code

--- ts/ts-classes-etc.js
+++ ts/ts-classes-etc.js
@@ -1,12 +1,12 @@
+"use strict";
 class Animal {
-    readonly id: number;
-    public name: string;
-    protected species: string;
+    id;
+    name;
+    species;
     // #sound: string;     // true private (ES2022 field)
-    // private sound: string;     // true private (ES2022 field)
-    #hashField: string;
-
-    constructor(name: string, species: string, hf: string) {
+    sound; // true private (ES2022 field)
+    #hashField;
+    constructor(name, species, hf) {
         this.id = Math.random();
         this.name = name;
         this.species = species;
@@ -18,33 +18,26 @@ const anml1 = new Animal('dog', 'dog specie', 'hashvalue');
 anml1.sound = 'another value';
 anml1.#hashField = 'abc';
 console.log(anml1);
-
-
 // class Animal {
 //     readonly id: number;
 //     public name: string;
 //     protected species: string;
 //     #sound: string;     // true private (ES2022 field)
-
 //     constructor(name: string, species: string) {
 //         this.id = Math.random();
 //         this.name = name;
 //         this.species = species;
 //         this.#sound = 'generic';
 //     }
-
 //     speak(): string {
 //         return `${this.name} says ${this.#sound}`;
 //     }
-
 //     get info(): string { return `${this.name} (${this.species})`; }
 //     set sound(value: string) { this.#sound = value; }
-
 //     static create(name: string, species: string): Animal {
 //         return new Animal(name, species);
 //     }
 // }
-
 // // // Parameter properties shorthand
 // // class Dog extends Animal {
 // //     constructor(
@@ -54,71 +47,46 @@ console.log(anml1);
 // //     ) {
 // //         super(name, 'dog');
 // //     }
-
 // //     speak(): string {
 // //         return `${this.name} barks!`;
 // //     }
 // // }
-
 // // // Implementing an interface
 // // interface Serializable {
 // //     toJSON(): object;
 // //     toString(): string;
 // // }
-
 // // class User implements Serializable {
 // //     constructor(public name: string, public email: string) { }
 // //     toJSON() { return { name: this.name, email: this.email }; }
 // //     toString() { return `${this.name} <${this.email}>`; }
 // // }
-
 // // // Abstract class
 // // abstract class Shape {
 // //     abstract area(): number;      // must be implemented by subclasses
 // //     abstract perimeter(): number;
-
 // //     describe(): string {
 // //         return `Area: ${this.area()}, Perimeter: ${this.perimeter()}`;
 // //     }
 // // }
-
 // // class Circle extends Shape {
 // //     constructor(private radius: number) { super(); }
 // //     area(): number { return Math.PI * this.radius ** 2; }
 // //     perimeter(): number { return 2 * Math.PI * this.radius; }
 // // }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // class Animal {
 //     id;
 //     name;
 //     species;
 //     #sound;
-
 //     constructor(id, name, species, sound) {
 //         this.id = id;
 //         this.name = name;
 //         this.species = species;
 //         this.#sound = sound;
 //     }
-
 //     printData() { console.log(this.#sound); };
 // }
-
 // const anml1 = new Animal(1, 'sona', 'abc', 'bark');
 // console.log(anml1);
 // console.log(anml1.#sound);
-
--- ts/ts-interfaces.ts
+++ ts/ts-interfaces.ts
@@ -1,38 +1,38 @@
-interface User {
-    readonly id: number;         // readonly: can't reassign
-    name: string;
-    email: string;
-    age?: number;                // optional
-    address?: {
-        city: string;
-        country: string;
-    };
-}
+// interface User {
+//     readonly id: number;         // readonly: can't reassign
+//     name: string;
+//     email: string;
+//     age?: number;                // optional
+//     address?: {
+//         city: string;
+//         country: string;
+//     };
+// }
 
-const user1: User = {
-    id: 1,
-    name: 'Sonu',
-    email: 'a@b.c'
-};
+// const user1: User = {
+//     id: 1,
+//     name: 'Sonu',
+//     email: 'a@b.c'
+// };
 
-console.log(user1);
-// user1.id = 2;// error 
-user1.name = 'Monu';
-console.log(user1);
+// console.log(user1);
+// // user1.id = 2;// error 
+// user1.name = 'Monu';
+// console.log(user1);
 
 
-// Extending interfaces
-interface AdminUser extends User {
-    role: 'admin';
-    permissions: string[];
-}
+// // Extending interfaces
+// interface AdminUser extends User {
+//     role: 'admin';
+//     permissions: string[];
+// }
 
-const adminUser: AdminUser = {
-    role: 'admin',
-    name: 'Tonu',
-    email: 'a',
-    id: 3,
-    permissions: []
+// const adminUser: AdminUser = {
+//     role: 'admin',
+//     name: 'Tonu',
+//     email: 'a',
+//     id: 3,
+//     permissions: []
 
-}
-console.log(adminUser);
+// }
+// console.log(adminUser);
